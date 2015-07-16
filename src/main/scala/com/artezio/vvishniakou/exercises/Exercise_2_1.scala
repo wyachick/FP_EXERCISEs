029efb3146d47c03c41a356ffe553af5b12474d2
wyachick <Viachaslau.Vishniakou@artezio.com>
2015-07-16 10:43:52 +0300

    Exercise 2.1

---
+++ src/main/scala/com/artezio/vvishniakou/exercises/Exercise_2_1.scala
@@ -0,0 +1,28 @@
+package com.artezio.vvishniakou.exercises
+
+object Exercise_2_1 extends App {
+
+  def fib(n: Int): Int = {
+
+    def go(n: Int, prev: Int, acc: Int): Int = {
+      if (n <= 1) {
+        acc + prev
+      } else {
+        go(n - 1, acc, acc + prev)
+      }
+    }
+    n match {
+      case x if (x <= 0) => 0
+      case 1 => 0
+      case 2 => 1
+      case _ => go(n-2, 0, 1)
+    }
+  }
+
+  println(fib(0))
+  println(fib(1))
+  println(fib(2))
+  println(fib(5))
+  println(fib(10))
+
+}
