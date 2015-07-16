f3101da4c79c8aa054d78cbbbab77b6dea48f6b1
wyachick <Viachaslau.Vishniakou@artezio.com>
2015-07-16 11:34:56 +0300

    Exercise 2.3

---
+++ src/main/scala/com/artezio/vvishniakou/exercises/Exercise_2_3.scala
@@ -0,0 +1,9 @@
+package com.artezio.vvishniakou.exercises
+
+object Exercise_2_3 extends App {
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  println(curry((a: Int, b: Int) => a > b)(5)(6))
+
+}
